b699f0d19f00cd2b7efcda6077e14a8aedee8535
SaimyoArata <arata-gamba4@docomo.ne.jp>
2024-06-07 23:22:32 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,54 +1,57 @@
-from flask import Flask, request, jsonify
-import cv2
-import io
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import base64
-import numpy as np
+from io import BytesIO
+from PIL import Image, UnidentifiedImageError
 
-app = Flask(__name__)
+app = FastAPI()
 
-def process_image(image_data):
-    # 画像データをバイナリデータにデコード
-    image_bytes = base64.b64decode(image_data)
-    np_arr = np.frombuffer(image_bytes, np.uint8)
-    image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
 
-    # 画像をグレースケールに変換（ここでは例としてグレースケールに変換しているが、任意の画像処理を行っても良い）
-    processed_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+origins = [
+    "http://localhost:3000",
+]
 
-    # 処理された画像をバイナリデータに変換
-    _, buffered = cv2.imencode('.jpg', processed_image)
-    encoded_image = base64.b64encode(buffered).decode('utf-8')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    return encoded_image
+class ImageData(BaseModel):
+    image: str
 
-@app.route('/upload', methods=['POST'])
-def upload():
+@app.post("/")
+async def upload_image(data: ImageData):
     try:
-        data = request.get_json()
-        image_data = data['image']
+        # Base64エンコードされた画像データをデコード
+        try:
+            print("Decoding image")
+            image_data = base64.b64decode(data.image.split(",")[1])  # 'data:image/jpeg;base64,' を除去
+        except base64.binascii.Error as decode_error:
+            raise HTTPException(status_code=400, detail="Invalid base64 string")
 
-        processed_image = process_image(image_data)
+        try:
+            print("Opening image")
+            image = Image.open(BytesIO(image_data))
+        except UnidentifiedImageError as img_error:
+            raise HTTPException(status_code=400, detail="Invalid image data")
 
-        return jsonify({'processed_image': processed_image})
+        # 画像をグレースケールに変換
+        print("Processing image")
+        processed_image = image.convert("L")
 
+        # 処理された画像をバイナリデータに変換
+        print("Encoding processed image")
+        buffered = BytesIO()
+        processed_image.save(buffered, format="JPEG")
+        encoded_image = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+        return {"message": "Image processed successfully", "processed_image": encoded_image}
     except Exception as e:
-        return jsonify({'error': str(e)}), 400
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5000)
-
-
-
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-
-# @app.get("/")
-# async def root():
-#     def hello_world():
-#         return "Hello World"
-#     # ここで関数を呼び出す
-#     result = hello_world()
-#     return {"message": result}
+        print(f"Error: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
